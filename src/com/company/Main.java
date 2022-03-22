b6a99f1926c8a0b25c350c6081b4118dda9d594c
RashiSonavani <rashisonavani123@gmail.com>
2022-03-22 22:13:18 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int min = 100, max =0;
+        Random r = new Random() ;
+        int nums[] = new int[10];
+
+        for(int i=0;i< nums.length ; i++){
+            nums[i]=Math.round((r.nextInt(100)+1 ));
+            System.out.print(nums[i] + ",");
+            if(nums[i]< min) min = nums[i];
+            if(nums[i]>max) max = nums [i];
+
+        }
+        System.out.println();
+        System.out.println(" the smallest value is:" + min);
+        System.out.println(" the greatest value is:" + max);
+
+    }
+}
